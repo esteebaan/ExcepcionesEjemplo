5403a5ac5024281a682e9c27f593262617a21d1f
USER <USER@DESKTOP-622UG1M>
2025-04-11 10:16:46 -0500

    Add project files.

---
+++ ExcepcionesEjemplo/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ExcepcionesEjemplo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(89, 69);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 0;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(89, 95);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 1;
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(248, 65);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 2;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(89, 150);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ ExcepcionesEjemplo/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcepcionesEjemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+
+                int numero1 = int.Parse(txtNumero1.Text);
+
+                int numero2 = int.Parse(txtNumero2.Text);
+
+
+
+                int resultado = numero1 / numero2;
+
+
+
+                lblResultado.Text = $"Resultado: {resultado}";
+
+            }
+
+            catch (FormatException)
+
+            {
+
+                MessageBox.Show("Por favor, introduce números válidos.", "Error de Formato",
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+            catch (DivideByZeroException)
+
+            {
+                lblResultado.Text = "Error";
+
+                MessageBox.Show("No es posible dividir entre cero.", "División por cero",
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show($"Error inesperado: {ex.Message}", "Error",
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            finally
+
+            {
+
+                txtNumero1.Clear();
+
+                txtNumero2.Clear();
+
+                txtNumero1.Focus();
+            }
+        }
+    }
+}
